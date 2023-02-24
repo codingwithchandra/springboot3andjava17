82e1bb50c679bb4044500a46727c21ed7efd5970
erchandrasekhar <er.chandrasekharyadav@gmail.com>
2023-02-24 19:12:10 +0530

    Spring boot and jdk 17  jsp demostration

---
+++ src/main/java/springboot/Springboot3Application.java
@@ -0,0 +1,14 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3Application.class, args);
+		
+	}
+
+}
